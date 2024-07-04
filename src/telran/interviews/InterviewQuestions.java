56c9076995f4860fc399372e00859604fdaa6bfa
Yuragranovsky <yuragranovsky@gmail.com>
2024-07-04 21:16:57 +0300

    CW #34 with TODO's comments for HW #34

--- src/telran/interviews/InterviewQuestions.java
+++ src/telran/interviews/InterviewQuestions.java
@@ -1,10 +1,20 @@
 package telran.interviews;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 public class InterviewQuestions {
 public static void displayOccurrences(String [] strings) {
 	HashMap<String, Integer> mapOccurrences = getOccurrencesMap(strings);
 	TreeMap<Integer, TreeSet<String>> treeMapOccurrences = getTreeMapOccurrences(mapOccurrences);
 	displayOccurrences(treeMapOccurrences);
+}
+public static void displayOccurrencesStream(String[] strings) {
+	Arrays.stream(strings)
+	.collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+	.entrySet().stream().sorted((e1, e2) -> {
+		int res = Long.compare(e2.getValue(), e1.getValue());
+		return res == 0 ? e1.getKey().compareTo(e2.getKey()) : res;
+	}).forEachOrdered(e -> System.out.printf("%s -> %d\n", e.getKey(), e.getValue()));
 }
 
 private static void displayOccurrences(TreeMap<Integer, TreeSet<String>> treeMapOccurrences) {
@@ -55,5 +65,37 @@ static public int getMaxWithNegativePresentation(int [] array) {
 		}
 	}
 	return maxRes;
+}
+public static Map<Integer, Integer> getMapSquares(List<Integer> numbers) {
+	Map<Integer, Integer> res = numbers.stream()
+			.collect(Collectors.toMap(n -> n, n -> n * n, (v1, v2) -> v1,
+					LinkedHashMap::new));
+	return res;
+}
+public static boolean isAnagram(String word, String anagram) {
+	//TODO
+	//returns true if "anagram" string contains all
+	// letters from "word" in another order (case sensitive)
+	//O[N] (sorting is disallowed)
+	return false;
+}
+public static List<DateRole> assignRoleDates(List<DateRole> rolesHistory,
+		List<LocalDate> dates) {
+	//TODO
+	//create List<DateRole> with roles matching with the given dates
+	//most effective data structure
+	return null;
+}
+public static void displayDigitsStatistics() {
+	//TODO
+	//display out statistics in the following format (example)
+	/* 1 -> <count of occurrences>
+	 * 2 -> .....
+	 * .........
+	 */
+	//sorted by counts of occurrences in the descending order
+	//takes 1000000 random numbers in range [0-Integer.MAX_VALUE)
+	//one pipeline with no additional yours methods
+	
 }
 }
--- src/telran/interviews/test/InterviewQuestionsTests.java
+++ src/telran/interviews/test/InterviewQuestionsTests.java
@@ -3,14 +3,39 @@ package telran.interviews.test;
 import static org.junit.jupiter.api.Assertions.*;
 import static telran.interviews.InterviewQuestions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import telran.interviews.DateRole;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
 class InterviewQuestionsTests {
 
+	private static final LocalDate DATE1 = LocalDate.of(2019, 12, 31);
+	private static final LocalDate DATE2 = LocalDate.of(2021, 10, 30);
+	private static final LocalDate DATE3 = LocalDate.of(2021, 12, 31);
+	private static final LocalDate DATE4 = LocalDate.of(2024, 1, 1);
+	private static final String ROLE1 = "Developer";
+	private static final String ROLE2 = "Senior Developer";
+	private static final String ROLE3 = "Manager";
 	@Test
 	void displayOccurrencesTest() {
 		String[] strings = {"b", "a","bb", "aa", "lmn", "aa", "lmn", "lmn", "bb"};
 		displayOccurrences(strings);
+		//Should be displayed out the following:
+		/*
+		 * lmn => 3
+		 * aa => 2
+		 * a => 1
+		 * b => 1
+		 */
+	}
+	@Test
+	void displayOccurrencesSteamTest() {
+		String[] strings = {"b", "a","bb", "aa", "lmn", "aa", "lmn", "lmn", "bb"};
+		displayOccurrencesStream(strings);
 		//Should be displayed out the following:
 		/*
 		 * lmn => 3
@@ -33,6 +58,64 @@ class InterviewQuestionsTests {
 		assertEquals(200, getMaxWithNegativePresentation(array1));
 		assertEquals(-1, getMaxWithNegativePresentation(array2));
 		assertEquals(200, getMaxWithNegativePresentation(array3));
+	}
+	@Test
+	@Disabled
+	void evenOddGroup() {
+		List<Integer> list = List.of(10, -3, 22, 41, 56, 7);
+		Map<String, List<Integer>> groupingMap = 
+		list.stream().collect(Collectors.groupingBy(n -> n % 2 == 0 ? "even" : "odd"));
+		System.out.println(groupingMap);
+			
+		
+	}
+	@Test
+	@Disabled
+	void evenOddCounts() {
+		List<Integer> list = List.of(10, -3, 22, 41, 56, 7, 3);
+		Map<String, Long> groupingMap = 
+				list.stream().collect(Collectors.groupingBy(n -> n % 2 == 0 ? "even" : "odd",
+						Collectors.counting()));
+		System.out.println(groupingMap);
+	}
+	@Test
+	void getSquaresMapTest() {
+		Integer[]numbers = {20, 10, -2, 5, 7, -2};
+		Map<Integer, Integer> actualMap = getMapSquares(List.of(numbers));
+		Integer[] expectedValues = {400, 100, 4, 25, 49};
+		Integer[] actualValues = actualMap.values().toArray(Integer[]::new);
+		assertArrayEquals(expectedValues, actualValues);
+	}
+	@Test
+	void isAnagrmaTest() {
+		String word = "hello";
+		assertTrue(isAnagram(word, "olleh"));
+		assertTrue(isAnagram(word, "elloh"));
+		assertTrue(isAnagram(word, "hleol"));
+		assertFalse(isAnagram(word, word));
+		assertFalse(isAnagram(word, "olle"));
+		assertFalse(isAnagram(word, "ollhh"));
+		assertFalse(isAnagram(word, "olehd"));
+	}
+	@Test
+	void datesRolesAssignmentTest() {
+		List<DateRole> history =
+				List.of(new DateRole(LocalDate.of(2020, 1, 1), ROLE1),
+						new DateRole(LocalDate.of(2021, 1, 1), ROLE2),
+						new DateRole(LocalDate.of(2022, 1, 1), ROLE3));
+		List<LocalDate> dates = List.of(DATE1,
+				DATE2, DATE3,
+				DATE4);
+		List<DateRole> expected =
+				List.of(new DateRole(DATE1, null), 
+						new DateRole(DATE2,ROLE2),
+						new DateRole(DATE3, ROLE2),
+						new DateRole(DATE4, ROLE3));
+		assertIterableEquals(expected, assignRoleDates(history, dates));
+	}
+	@Test
+	void displayDigitsStatisticsTest() {
+		displayDigitsStatistics();
 	}
 
 }
